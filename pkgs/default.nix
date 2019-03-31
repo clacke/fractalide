b6d99fea0e11eeb3221638b5dc4f6ebdcc7a2b0c
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2019-03-31 15:04:52 +0800

    WIP: fractalide retested when fbp-lang changes
    Filter.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -40,6 +40,7 @@ pkgs {
         filter = (path: type:
           let basePath = baseNameOf path; in
           (null != builtins.match ((toString ./..) + "(/info.rkt|/(modules|edges|nodes)(/rkt(/.*)?)?)") path) &&
+          (null == builtins.match ((toString ./..) + "/modules.rkt/fbp-lang/.*") path) &&
           (type != "symlink" || null == builtins.match "result.*" basePath) &&
           (null == builtins.match ".*[.]nix" basePath) &&
           (null == builtins.match "[.].*[.]swp" basePath) &&
